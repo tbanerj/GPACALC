1cba9d52e5e5c2e0fab9660ff4577a92017f4c01
tbanerj <64343096+tbanerj@users.noreply.github.com>
2024-04-06 14:07:38 -0700

    re do

--- proj/gradebook/gpaCalc/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-class Class(models.Model):
-    userID = models.CharField(max_length = 200)
-    name = models.CharField(max_length=200)
-    letterGrade = models.CharField(max_length = 1)
